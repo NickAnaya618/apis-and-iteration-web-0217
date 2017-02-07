7238905254eb9d92a66b45be0e64457925076c35
NickAnaya618 <anaya.nicholas@gmail.com>
2017-02-07 17:11:25 -0500

    Submitting convoluted version of apis-and-iteration-web-0217 lab

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -1,25 +1,44 @@
 require 'rest-client'
 require 'json'
 require 'pry'
+# iterate over the character hash to find the collection of `films` for the given
+#   `character`
+# && character == character_hash["results"][index]["name"].downcase
 
 def get_character_movies_from_api(character)
   #make the web request
-  all_characters = RestClient.get('http://www.swapi.co/api/people/')
-  character_hash = JSON.parse(all_characters)
-  
-  # iterate over the character hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
+  i = 1
+  final_array = []
+  while i < 10
+      all_characters = RestClient.get('http://www.swapi.co/api/people/' + "?page=#{i}")
+      character_hash = JSON.parse(all_characters)
+      final_array << character_hash["results"]
+    i += 1
+  end
+
+  final_array = final_array.flatten
+
+  film_collection = []
+
+  final_array.each_with_index do |char_hash, index|
+    if character == final_array[index]["name"].downcase
+      film_collection << final_array[index]["films"]
+    end
+  end
+
+  film_collection = film_collection.flatten
+
+  film_collection.collect do |film_url|
+      all_films = RestClient.get(film_url)
+      films_hash = JSON.parse(all_films)
+  end
 end
 
 def parse_character_movies(films_hash)
   # some iteration magic and puts out the movies in a nice list
+  films_hash.each_with_index do |movie_hash, index|
+    puts films_hash[index]["title"]
+  end
 end
 
 def show_character_movies(character)
